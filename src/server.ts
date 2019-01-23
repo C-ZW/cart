2da96852567f401d52e5c847c1b62cbbbdd4eb37
C-ZW <a19575091@gmail.com>
2019-01-23 18:10:48 +0800

    separate app for testing

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,4 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import User from './route/user';
-import Cart from './route/cart';
-import Register from './route/register';
-
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
-
-app.use('/api', new User().router);
-app.use('/api', new Cart().router);
-app.use('/api', new Register().router);
+import app from './app';
 
 
 const PORT = 8000;
