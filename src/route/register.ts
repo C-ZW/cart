0359b317ddb6c101d9ee43360a323b2eeaf115d6
C-ZW <a19575091@gmail.com>
2019-01-23 20:50:00 +0800

    add implementation

--- src/route/register.ts
+++ src/route/register.ts
@@ -1,5 +1,10 @@
 import * as express from 'express';
-import tables from '../db/pgdb';
+import { v4 as uuid } from 'uuid/v4'
+import * as sequelize from 'sequelize';
+
+import db from '../db/pgdb';
+import hash from '../util/hash';
+
 
 export default class Register {
     public router = express.Router();
@@ -9,6 +14,29 @@ export default class Register {
     }
 
     private async register(req: express.Request, res: express.Response) {
+        let userId = uuid()
+        try {
+            await db.sequelize.transaction(async (t) => {
+                await db.tables.user.create({
+                    id: userId,
+                    account: req.body.account,
+                    password: hash(req.body.password)
+                }, { transaction: t })
 
+                await db.tables.user_profile.create({
+                    user_id: userId,
+                    name: req.body.name,
+                    credit: req.body.credit,
+                    created_time: new Date(),
+                    last_login_time: new Date()
+                }, {
+                        transaction: t
+                    });
+            });
+        } catch (err) {
+
+        }
+
+        res.end();
     }
 }
